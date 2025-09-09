1722ec7d16619539bc6943d1733ce8dd8e708b23
TheFlappyPatty <75271177+TheFlappyPatty@users.noreply.github.com>
2025-09-08 17:11:39 -0700

    test 1

--- Project scuba/Assets/scripts/RopsManager.cs
+++ Project scuba/Assets/scripts/RopsManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class RopsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    public Transform ropeStart;
+    public GameObject rope;
+    public float Ropelength;
+    public GameObject prevconected;
+
+       void Start()
     {
-        
+
+        for (int i = 0; i < Ropelength; i++)
+        {
+            GameObject a = Instantiate(rope, ropeStart.position + new Vector3(0,-i/2,0), Quaternion.identity, ropeStart);
+            if (prevconected != null) a.GetComponent<HingeJoint2D>().connectedBody = prevconected.GetComponent<Rigidbody2D>();
+            prevconected = a;
+
+        }
+        player.GetComponent<HingeJoint2D>().connectedBody = prevconected.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void addRope()
     {
-        
+        GameObject a = Instantiate(rope, ropeStart.position + new Vector3(0, prevconected.transform.position.y/3, 0), Quaternion.identity, ropeStart);
+        if (prevconected != null) a.GetComponent<HingeJoint2D>().connectedBody = prevconected.GetComponent<Rigidbody2D>();
+        prevconected = a;
+    }
+
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            addRope();
+        }
     }
 }
